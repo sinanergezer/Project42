356abdb00364a17212d4f2e697bb911cffb6fef2
Atalay Korkut <atalaykorkut@hotmail.com>
2021-03-24 01:17:21 +0300

    routing app express
    routing app express

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const indexRouteController=require('./routes/indexRoute');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World 2');
-});
+const server = http.createServer(app);
+  
 app.use('/', indexRouteController);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
